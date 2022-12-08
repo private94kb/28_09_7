721268b089a6334910722837f4de7eb1dbaf2f74
private94kb <91588734+private94kb@users.noreply.github.com>
2022-12-08 18:50:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+ #include <cmath>
+
+using namespace std; 
+
+int main(void){ 
+
+    float val1= 2.3; 
+
+    float val2= 2.3; 
+
+    float val3= 2.123456; 
+
+    float val4= 2.123456; 
+
+    float val5= 2.123456; 
+
+     
+
+    cout << val1 <<'\n';// 
+
+    cout << val2 <<"0"<<'\n';// value + 0
+
+    cout << val3 <<'\n';// 
+
+    cout << round (val4 * 100)/100 <<'\n';// округлюю до другого знаку після коми  
+
+    cout << int (val5) <<'\n';// перетворення з float у int
+
+} 
+
+ 
